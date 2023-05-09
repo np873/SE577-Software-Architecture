bdc977e0a66eeb595fb2ac9b9ff18009bdb5a4de
Niki Patel <patel.nik95@gmail.com>
2023-05-09 12:55:42 -0400

    Final Change

--- microservices/server.ts
+++ microservices/server.ts
@@ -27,6 +27,44 @@ const yamlData = YAML.parse(file)
     reply.code(200).send(yamlData.repoDetails)
 })
 
+interface Repo {
+  id: string;
+  name: string;
+  url: string;
+  language: string;
+  updated: string;
+}
+
+interface RepoData {
+  repoDetails: Repo[];
+}
+
+interface Params {
+  id: string
+}
+
+server.get('/repoDetails/:id', async (request, reply) => {
+
+  const file = fs.readFileSync('./repoDetails.yaml', 'utf8');
+  const yamlData = YAML.parse(file) as RepoData;
+
+  if (yamlData == undefined) {
+    reply.code(400).send('Error no repos defined');
+    return;
+  }
+
+  const { id } = request.params as Params;
+  const repo = yamlData.repoDetails.find((r: Repo) => r.id == id); 
+
+  if (repo == undefined) {
+    reply.code(404).send(`Repo with id ${id} not found`);
+    return;
+  }
+
+  reply.code(200).send(repo);
+});
+
+
 server.listen(5087, (err, address) => {
   if (err) {
     console.error(err)
--- src/pages/Repos.vue
+++ src/pages/Repos.vue
@@ -1,25 +1,30 @@
 <template>
-        <p class="text-center"> This page shows the list of repos and repos details</p>
+  <div>
+    <p class="text-center" style="font-family: Arial; font-size: 20px; color: blueviolet;">
+      This page shows the list of repos and repo details
+    </p>
 
     <div v-if="repoData.length > 0">
-    <table>
-      <tr>
-        <th>Repo ID</th>
-        <th>Repo Name</th>
-        <th>Url</th>
-        <th>Language</th>
-        <th>Last Updated</th>
-      </tr>
-      <tr v-for = "(repo, rowNum) in repoData">
-        <td>{{ repo.id }}</td>
-        <td>{{ repo.name }}</td>
-        <td>{{ repo.url }}</td>
-        <td>{{ repo.language }}</td>
-        <td>{{ repo.updated }}</td>
-      </tr>
-    </table>
+      <table border style="margin: 0 auto;">
+        <tr style="background-color: blueviolet;">
+          <th style="padding: 10px; font-family: Arial; font-size: 16px; color: white;">Repo ID</th>
+          <th style="padding: 10px; font-family: Arial; font-size: 16px; color: white;">Repo Name</th>
+          <th style="padding: 10px; font-family: Arial; font-size: 16px; color: white;">Url</th>
+          <th style="padding: 10px; font-family: Arial; font-size: 16px; color: white;">Language</th>
+          <th style="padding: 10px; font-family: Arial; font-size: 16px; color: white;">Last Updated</th>
+        </tr>
+        <tr v-for="(repo, rowNum) in repoData" :key="rowNum" :style="(rowNum % 2 == 0) ? {backgroundColor: '#f2f2f2'} : {}">
+          <td style="padding: 10px; font-family: Arial; font-size: 14px;">{{ repo.id }}</td>
+          <td style="padding: 10px; font-family: Arial; font-size: 14px;">{{ repo.name }}</td>
+          <td style="padding: 10px; font-family: Arial; font-size: 14px;">{{ repo.url }}</td>
+          <td style="padding: 10px; font-family: Arial; font-size: 14px;">{{ repo.language }}</td>
+          <td style="padding: 10px; font-family: Arial; font-size: 14px;">{{ repo.updated }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
+
     
     <script lang="ts">
     export default {
@@ -37,14 +42,13 @@
     onMounted(async () => {
       console.log("Page 1 mounted")
 
-      //this is where to go and get the student data
       let allRepoURL = 'http://localhost:5087/repoDetails'
 
-      //Use axios to load the student data - readup on await to make
+      //Use axios to load the repo data - readup on await to make
       //async calls easier
       let repoAPI = await axios.get<RepoApiInterface[]>(allRepoURL)
 
-      //if OK, set the studentData variable, so that we can render in the ui
+      //if OK, set the repoData variable, so that we can render in the ui
       if(repoAPI.status == 200){
         repoData.value = repoAPI.data
       }
